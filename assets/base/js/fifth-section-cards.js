ce06d1abd2bbf46a77ad0c2c4f7139ab19f7463b
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-06-19 11:01:39 +0430

    added structure of fifth section

---
+++ assets/base/js/fifth-section-cards.js
@@ -0,0 +1,10 @@
+// Codes By Mahdi Tasha
+// Some Settings For Cards Of Fifth Section Of Home Page
+const swiper = new Swiper(".dv-card-swiper", {
+    effect: "cards",
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+});
